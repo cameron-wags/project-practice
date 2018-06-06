00d5e34318988d9c4235f7da84b02e8f1e2fee22
Daniel Hardy <danielhardy302@gmail.com>
2018-06-06 14:52:37 -0400

    fixed argument names and added test

--- practice-api/practice-api/Controllers/MultiplyController.cs
+++ practice-api/practice-api/Controllers/MultiplyController.cs
@@ -13,10 +13,10 @@ namespace practice_api.Controllers
     public class MultiplyController : Controller
     {
         [HttpGet]
-        public string Get([FromQuery] string arg1, [FromQuery] string arg2)
+        public string Get([FromQuery] string value1, [FromQuery] string value2)
         {
 
-            return (int.Parse(arg1) * int.Parse(arg2)).ToString();
+            return (int.Parse(value1) * int.Parse(value2)).ToString();
         }
 
     }
